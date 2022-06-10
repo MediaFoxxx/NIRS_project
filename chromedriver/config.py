b77ad346a3528dc56c7fa2f06a593f87541b6722
MediaFoxxx <bushuev4329@yandex.ru>
2022-06-10 15:05:58 +0300

    Preparing for heroku

---
+++ chromedriver/config.py
@@ -0,0 +1,9 @@
+group_domain = 'decanat_rk'
+group_id = -56443935
+chat_id = "@decanat_rk"
+bot_token = "5262146244:AAFELyFupCJ42On-F7eCAmyqxnfiTtIdFr0"
+vk_token = "eef85a7cb86c9b19b4b8af03b3d07565e7f9c4707dd4e722463f3008c4aacbc9ff3cd259e690beeb59258"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98"
+                  ".0.4758.119 YaBrowser/22.3.0.2430 Yowser/2.5 Safari/537.36"
+}
